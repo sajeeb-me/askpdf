ff5aeaf290a568c2c05758a41a1297948de537b0
Sajeeb Das Shuvo <hellosdshuvo@gmail.com>
2024-07-30 10:31:49 +0100

    chore: Update npm dependencies and add react-loading-skeleton

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -2,8 +2,10 @@
 
 import { trpc } from "@/app/_trpc/client"
 import UploadButton from "./UploadButton"
-import { Ghost } from "lucide-react"
+import { Ghost, Loader2, MessageSquare, Plus, Trash } from "lucide-react"
 import Skeleton from 'react-loading-skeleton'
+import Link from "next/link"
+import { Button } from "./ui/button"
 
 const Dashboard = () => {
 
@@ -19,64 +21,42 @@ const Dashboard = () => {
             </div>
 
             {files && files?.length !== 0 ? (
-                <div></div>
-                // <ul className='mt-8 grid grid-cols-1 gap-6 divide-y divide-zinc-200 md:grid-cols-2 lg:grid-cols-3'>
-                //     {files
-                //         .sort(
-                //             (a, b) =>
-                //                 new Date(b.createdAt).getTime() -
-                //                 new Date(a.createdAt).getTime()
-                //         )
-                //         .map((file) => (
-                //             <li
-                //                 key={file.id}
-                //                 className='col-span-1 divide-y divide-gray-200 rounded-lg bg-white shadow transition hover:shadow-lg'>
-                //                 <Link
-                //                     href={`/dashboard/${file.id}`}
-                //                     className='flex flex-col gap-2'>
-                //                     <div className='pt-6 px-6 flex w-full items-center justify-between space-x-6'>
-                //                         <div className='h-10 w-10 flex-shrink-0 rounded-full bg-gradient-to-r from-cyan-500 to-blue-500' />
-                //                         <div className='flex-1 truncate'>
-                //                             <div className='flex items-center space-x-3'>
-                //                                 <h3 className='truncate text-lg font-medium text-zinc-900'>
-                //                                     {file.name}
-                //                                 </h3>
-                //                             </div>
-                //                         </div>
-                //                     </div>
-                //                 </Link>
+                <ul className='mt-8 grid grid-cols-1 gap-6 divide-y divide-zinc-200 md:grid-cols-2 lg:grid-cols-3'>
+                    {files
+                        .sort(
+                            (a: any, b: any) =>  // TODO: fix any
+                                new Date(b.createdAt).getTime() -
+                                new Date(a.createdAt).getTime()
+                        )
+                        .map((file: any) => (    // TODO: fix any
+                            <li
+                                key={file.id}
+                                className='col-span-1 divide-y divide-gray-200 rounded-lg bg-white shadow transition hover:shadow-lg'>
+                                <Link
+                                    href={`/dashboard/${file.id}`}
+                                    className='flex flex-col gap-2'>
+                                    <div className='pt-6 px-6 flex w-full items-center justify-between space-x-6'>
+                                        <div className='h-10 w-10 flex-shrink-0 rounded-full bg-gradient-to-r from-emerald-500 to-green-500' />
+                                        <div className='flex-1 truncate'>
+                                            <div className='flex items-center space-x-3'>
+                                                <h3 className='truncate text-lg font-medium text-zinc-900'>
+                                                    {file.name}
+                                                </h3>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </Link>
 
-                //                 <div className='px-6 mt-4 grid grid-cols-3 place-items-center py-2 gap-6 text-xs text-zinc-500'>
-                //                     <div className='flex items-center gap-2'>
-                //                         <Plus className='h-4 w-4' />
-                //                         {format(
-                //                             new Date(file.createdAt),
-                //                             'MMM yyyy'
-                //                         )}
-                //                     </div>
+                                <div className='px-6 mt-4 grid grid-cols-3 place-items-center py-2 gap-6 text-xs text-zinc-500'>
 
-                //                     <div className='flex items-center gap-2'>
-                //                         <MessageSquare className='h-4 w-4' />
-                //                         mocked
-                //                     </div>
-
-                //                     <Button
-                //                         onClick={() =>
-                //                             deleteFile({ id: file.id })
-                //                         }
-                //                         size='sm'
-                //                         className='w-full'
-                //                         variant='destructive'>
-                //                         {currentlyDeletingFile === file.id ? (
-                //                             <Loader2 className='h-4 w-4 animate-spin' />
-                //                         ) : (
-                //                             <Trash className='h-4 w-4' />
-                //                         )}
-                //                     </Button>
-                //                 </div>
-                //             </li>
-                //         ))}
-                // </ul>
+                                    <div className='flex items-center gap-2'>
+                                        <MessageSquare className='h-4 w-4' />
+                                        mocked
+                                    </div>
+                                </div>
+                            </li>
+                        ))}
+                </ul>
             ) : isLoading ? (
                 <Skeleton height={100} className='my-2' count={3} />
             ) : (
