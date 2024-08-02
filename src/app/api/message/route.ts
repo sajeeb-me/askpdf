cd18ca0baecf0402c62a6209cf1b4661fd6239ba
Sajeeb Das Shuvo <hellosdshuvo@gmail.com>
2024-08-02 11:26:08 +0100

    feat: Add file upload handling using uploadthing/next library
    Integrate the uploadthing/next library to handle file uploads in the src/app/api/uploadthing/route.ts file. This feature enhancement improves the application's ability to handle file uploads and integrates with the existing ourFileRouter.

--- src/app/api/message/route.ts
+++ src/app/api/message/route.ts
@@ -1,3 +1,5 @@
+import { db } from "@/db";
+import { SendMessageValidator } from "@/lib/validators/SendMessageValidator";
 import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 import { NextRequest } from "next/server";
 
@@ -9,11 +11,29 @@ export const POST = async (req: NextRequest) => {
     const { getUser } = getKindeServerSession();
     const user = await getUser();
 
-    // const userId = user?.id;
-    const { id: userId } = user;
+    const userId = user?.id;
+    // const { id: userId } = user;
 
     if (!userId)
         return new Response('Unauthorized', { status: 401 });
 
-    const{}
+    const { fileId, message } = SendMessageValidator.parse(body);
+
+    const file = await db.file.findFirst({
+        where: {
+            id: fileId,
+            userId,
+        }
+    });
+
+    if (!file)
+        return new Response('File not found', { status: 404 });
+
+    await db.message.create({
+        data: {
+            fileId,
+            message,
+            userId,
+        }
+    });
 }
