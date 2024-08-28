7948bfc4eeba6811f157b99e1cc4dcb6ce55b409
Sajeeb Das Shuvo <hellosdshuvo@gmail.com>
2024-08-28 11:24:52 +0100

    feat: Update @pinecone-database/pinecone dependency to version 3.0.2
    The code changes in the package-lock.json and package.json files update the @pinecone-database/pinecone dependency from version 3.0.0 to version 3.0.2. This update ensures compatibility with the latest version of @pinecone-database/pinecone and may include bug fixes or performance improvements.

--- src/lib/pinecone.ts
+++ src/lib/pinecone.ts
@@ -1,12 +1,18 @@
-import { PineconeClient } from '@pinecone-database/pinecone'
+import { Pinecone } from '@pinecone-database/pinecone'
 
-export const getPineconeClient = async () => {
-  const client = new PineconeClient()
+export const pinecone = new Pinecone({
+  apiKey: process.env.PINECONE_API_KEY!,
+  // environment: 'us-east1-gcp',
+  environment: 'us-east-1',
+})
 
-  await client.init({
-    apiKey: process.env.PINECONE_API_KEY!,
-    environment: 'us-east1-gcp',
-  })
+// export const getPineconeClient = async () => {
+//   const client = new PineconeClient()
 
-  return client
-}
+//   await client.init({
+//     apiKey: process.env.PINECONE_API_KEY!,
+//     environment: 'us-east1-gcp',
+//   })
+
+//   return client
+// }
